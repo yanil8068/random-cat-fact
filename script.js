0fd48bbd0c9bfe5b37d94148cbbb3c70cca415e2
yanil8068 <yanil8068@gmail.com>
2023-11-21 15:17:00 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// for showing fact
 let btn1 = document.querySelector("#btn1");
 btn1.addEventListener('click', async()=>{
     let fact = await getfacts();
-    let p = document.querySelector("#result");
+    let p = document.querySelector("#result1");
     // for image we do img.setAttribute('src', link);
     p.innerText = fact ;
 })
@@ -12,10 +13,33 @@ async function getfacts(){
         let res= await axios.get(url1);
         return res.data.fact;
     }catch(e){
-      
-        return "No fact sound";
+
+        return "No fact found";
     }
 }
+
+
+// for showing image
+let btn2 = document.querySelector("#btn2");
+btn2.addEventListener('click', async()=>{
+    let image = await showimage();
+    let result2 = document.querySelector("#result2");
+    // for image we do img.setAttribute('src', link);
+    result2.setAttribute('src', image) ;
+})
+
+let url2="https://dog.ceo/api/breeds/image/random";
+async function showimage(){
+    try{
+        let res= await axios.get(url2);
+        return res.data.message;
+    }catch(e){
+
+        return "No fact found";
+    }
+}
+
+
 
 
 
